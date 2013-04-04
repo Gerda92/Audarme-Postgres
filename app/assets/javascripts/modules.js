a60726879e1c80edd61f1a994c9f52c941038ffc
Super <super@super-Aspire-5750G.(none)>
2013-04-04 18:26:59 +0600

    Hello Gerda

--- app/assets/javascripts/modules.js
+++ app/assets/javascripts/modules.js
@@ -1,5 +1,10 @@
 var modules = {}
 var sandbox = {}
+var global = {
+	lan : ["ru", "kz"],
+	current : 0
+}
+
 /**
 * Module initializing
 */
@@ -103,6 +108,7 @@ modules["modal-module"] = (function(){
 	_contstructor_(); 
 });
 modules["language-change"] = (function(){
+		
 	var _init_ = function(){
 		var btn = $("#js-swap-lan");
 		btn.bind("click", function(){
@@ -110,6 +116,9 @@ modules["language-change"] = (function(){
 			var b = $("#js-swap-2").html();
 			$("#js-swap-1").html(b);
 			$("#js-swap-2").html(s);
+			
+			global.current = (global.current + 1) % global.lan.length;
+
 		});
 	}
 	_init_.call();
@@ -125,19 +134,63 @@ modules["enter-button"] = (function(){
 	var _init = function(){
 
 		_data_.getButton.bind("click", function(){
-			var vl = _data_.getInput.val();
-			var url = "/words/kz/" + vl;
-			$.ajax({
-				url : url,
-				type : "GET",
-				success : function(_json_){
-					alert(_json_);
-				}
-			});
+			action();
 		});
 
+		_data_.getInput.bind("keydown", function(ev){
+			if(ev.keyCode == 13){
+				//pressed enter
+				action();
+			}
+			else{
+				//32 27 8 9
+				var list_none = [32, 27, 9, 18];
+				var st = true;
+				for(var i = 0; i < list_none.length; i++){
+					if(ev.keyCode == list_none[i]){
+						st = false;
+						break;
+					}
+				}
+				if(st){
+					//continue and wait 200 secunds
+					setTimeout(load_suggest, 200);
+				}
+			}
+		});
 	}
 
+	var action = function(){
+		var vl = _data_.getInput.val();
+		var url = "/words/" + global.lan[global.current] + "/" + vl;
+		$.ajax({
+			url : url,
+			type : "GET",
+			dataType: "JSON",
+			success : function(_json_){
+				var object = _json_ || {};
+				var title = $("#js-word-title");
+				var content = $("#js-word-content");
+				title.html(vl);
+				content.html(object.definition);
+				//document.location.replace(url);
+			},
+			error : function(){
+				alert("Ваше слово не найдено!");
+			}
+		});
+	}
+	var load_suggest = function(){
+		var vl = _data_.getInput.val();
+		var url = "../suggest/" + global.lan[global.current] + "/" + vl;
+		//loading some ajax
+		$.ajax({
+			url : url,
+			success : function(_json_){
+				console.log(_json_);
+			}
+		});
+	}
 	//the queue of actions
 	_init.call();
 });
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -12,6 +12,10 @@ class WordsController < ApplicationController
     @word = Word.where(:language => params['lang'], :name => params['name']).first
     other_lang = params['lang'] == 'kz'? 'ru' : 'kz'
     @word.definition.gsub!(/(<a[^>]*href=")[^"]*("[^>]*>)(.*)(<\/a>)/ix, '\1/words/'+ other_lang +'/\3\2\3\4')
+    respond_to do |format|
+      format.html { render :define }
+      format.json { render :json => @word }
+    end
   end
 
   def suggest
